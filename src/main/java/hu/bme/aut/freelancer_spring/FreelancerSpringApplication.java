e2c5896825e0cf610c27d79f7dfaacce70f0b11d
akos989 <48367888+akos989@users.noreply.github.com>
2020-10-26 13:54:55 +0100

    Revert "db configurations"
    This reverts commit 0fd4c721ba44a1257499d911bff1a195af035051.

--- src/main/java/hu/bme/aut/freelancer_spring/FreelancerSpringApplication.java
+++ src/main/java/hu/bme/aut/freelancer_spring/FreelancerSpringApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FreelancerSpringApplication{
+public class FreelancerSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FreelancerSpringApplication.class, args);
