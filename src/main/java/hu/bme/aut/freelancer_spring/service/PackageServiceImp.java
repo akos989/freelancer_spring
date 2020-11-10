57a077b19b1bf63f6d63377db27631d679ef5995
akos989 <morvaiakos1998@gmail.com>
2020-11-10 15:07:16 +0100

    error handling: throw ResponseStatusException in services

--- src/main/java/hu/bme/aut/freelancer_spring/service/PackageServiceImp.java
+++ src/main/java/hu/bme/aut/freelancer_spring/service/PackageServiceImp.java
@@ -5,14 +5,11 @@ import hu.bme.aut.freelancer_spring.model.Package;
 import hu.bme.aut.freelancer_spring.model.Transfer;
 import hu.bme.aut.freelancer_spring.model.enums.Status;
 import hu.bme.aut.freelancer_spring.repository.PackageRepository;
-import hu.bme.aut.freelancer_spring.repository.TownRepository;
 import hu.bme.aut.freelancer_spring.repository.TransferRepository;
 import hu.bme.aut.freelancer_spring.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,7 +22,6 @@ public class PackageServiceImp implements PackageService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper = new ModelMapper();
     private final TransferRepository transferRepository;
-    private final TownRepository townRepository;
 
     @Override
     public List<Package> findAll() {
@@ -35,38 +31,46 @@ public class PackageServiceImp implements PackageService {
     @Override
     public Long save(PackageDto packageDto) {
         var sender = userRepository.findById(packageDto.getSenderId());
-        var town = townRepository.findById(packageDto.getTownId());
-        if (sender.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Sender was not found with id: " + packageDto.getSenderId());
-        if (town.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Town was not found with id: " + packageDto.getTownId());
-
-        Package pack = modelMapper.map(packageDto, Package.class);
-        findTransfer(pack).ifPresent(pack::setTransfer);
-        packageRepository.save(pack);
-        return pack.getId();
+        if (sender.isPresent()) {
+            Package pack = modelMapper.map(packageDto, Package.class);
+            findTransfer(pack).ifPresent(pack::setTransfer);
+            packageRepository.save(pack);
+            return pack.getId();
+        } else {
+            return null;
+        }
     }
 
     @Override
     public boolean delete(Long id) {
         var pack = packageRepository.findById(id);
-        if (pack.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Package was not found with id: " + id);
+        if (pack.isPresent()) {
+            packageRepository.delete(pack.get());
+            return true;
+        } else {
+            return false;
         }
-        packageRepository.delete(pack.get());
-        return true;
+    }
+
+    @Override
+    public boolean updateTransferId(Long packageId, Long transferId) {
+        //TODO
+//        var p = packageRepository.findById(packageId);
+//        p.
+        return false;
     }
 
     @Override
     public boolean changeStatus(Long packageId, Status status) {
         var pack = packageRepository.findById(packageId);
-        if (pack.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Package was not found with id: " + packageId);
+        if (pack.isPresent()) {
+            var p = pack.get();
+            p.setStatus(status);
+            packageRepository.save(p);
+            return true;
+        } else {
+            return false;
         }
-        var p = pack.get();
-        p.setStatus(status);
-        packageRepository.save(p);
-        return true;
     }
 
     private Optional<Transfer> findTransfer(Package pack) {
