cdc147f152b7f89f60c1398ff76de59988b99501
akos989 <morvaiakos1998@gmail.com>
2020-11-19 12:26:02 +0100

    transfer controller refactor

--- src/main/java/hu/bme/aut/freelancer_spring/controller/TransferController.java
+++ src/main/java/hu/bme/aut/freelancer_spring/controller/TransferController.java
@@ -33,8 +33,8 @@ public class TransferController {
         return ResponseEntity.ok(transferService.delete(id));
     }
 
-    @GetMapping("/packages/{id}")
-    ResponseEntity<List<Package>> getTransfers(@PathVariable Long id) {
+    @GetMapping("/{id}/packages")
+    ResponseEntity<List<Package>> getPackages(@PathVariable Long id) {
         return ResponseEntity.ok(transferService.getPackages(id));
     }
 
